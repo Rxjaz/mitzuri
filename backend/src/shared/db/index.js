f0ee0aca197bb19da58fe287ee4215db6e9199da
Rxjaz <rg.rxjaz04@gmail.com>
2026-04-19 21:08:08 -0600

    fix railway and docker

--- backend/src/shared/db/index.js
+++ backend/src/shared/db/index.js
@@ -1,14 +1,30 @@
 import pkg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
+
+export const testDBConnection = async () => {
+  try {
+    const res = await pool.query('SELECT NOW()');
+    console.log('DB OK:', res.rows);
+  } catch (err) {
+    console.error('DB ERROR:', err);
+  }
+};
