0b7c3b978728c6e29e347beb729a1f6b01398388
Rxjaz <rg.rxjaz04@gmail.com>
2026-04-24 13:53:22 -0600

    include cloudflare for media

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,30 +1,16 @@
 import express from 'express';
 import { pool } from './shared/db/index.js';
+//import mediaRoutes from './modules/media/media.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
+//app.use('/admin/media', mediaRoutes);
+
 app.get('/', (req, res) => {
-  res.send('API running');
+  res.send('Sexooooo');
 });
 
-app.get('/test-db', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.json(result.rows[0]);
-  } catch (error) {
-    console.error('DB ERROR:', {
-      code: error.code,
-      message: error.message,
-      stack: error.stack,
-    });
-    res.status(500).json({
-      error: 'DB error',
-      code: error.code,
-      message: error.message,
-    });
-  }
-});
 
 export default app;
