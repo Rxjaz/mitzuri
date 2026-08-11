caea5a0de67adb9de4bdcbe93b2bfd4c630ed928
Rxjaz <rg.rxjaz04@gmail.com>
2026-08-11 08:42:09 -0600

    chore(lab): maqueta temporal de feed publico

--- frontend/src/app/public/routes.tsx
+++ frontend/src/app/public/routes.tsx
@@ -1,11 +1,18 @@
 import { Route } from "react-router-dom";
 import PublicLayout from "./layout/PublicLayout";
 import HomePage from "./pages/HomePage";
+import FeedLabPage from "./pages/FeedLabPage";
 
 export function PublicRoutes() {
   return (
-    <Route path="/" element={<PublicLayout />}>
-      <Route index element={<HomePage />} />
-    </Route>
+    <>
+      <Route path="/" element={<PublicLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+
+      {/* TEMPORAL: maqueta de diseno, fuera de PublicLayout porque define
+          su propio header y necesita ancho completo */}
+      <Route path="/lab" element={<FeedLabPage />} />
+    </>
   );
 }
